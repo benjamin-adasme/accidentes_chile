38dcf91e23a076aa5185c4e86f11d30e112cb8f0
BAdasme_UCM <badasme@ucm.cl>
2023-08-17 15:24:15 -0400

    cambios 17-08

--- code/descarga por años.R
+++ code/descarga por años.R
@@ -3,24 +3,30 @@ pacman::p_load(readxl,
                tidyverse,
                readxlsb)
 
-num <- 1:12
-
-path <- "https://www.carabineros.cl/transparencia/tproactiva/OS2/os2_acc_2022_"
-
-data_list_2 <- list()
-
-for (i in num) {
-  mes_num <- ifelse(i < 10, paste0("0",i), as.character(i))
-  urls <- paste0(path, mes_num, ".xlsx")
-  p1 <- tempfile(fileext = ".xlsx")
-  download.file(urls, p1, mode = "wb")
-  data_list_2[[i]] <- readxl::read_xlsx(path = p1, na = "NULL")
-}
+# num <- 1:12
+# 
+# path <- "https://www.carabineros.cl/transparencia/tproactiva/OS2/os2_acc_2022_"
+# 
+# data_list_2 <- list()
+# 
+# for (i in num) {
+#   mes_num <- ifelse(i < 10, paste0("0",i), as.character(i))
+#   urls <- paste0(path, mes_num, ".xlsx")
+#   p1 <- tempfile(fileext = ".xlsx")
+#   download.file(urls, p1, mode = "wb")
+#   data_list_2[[i]] <- readxl::read_xlsx(path = p1, na = "NULL")
+# }
 
 
 # for(i in 1:length(data_list_2)) {
 #   assign(paste0("db",i), data_list_2[[i]])
 # }
+
+
+# Reporte por tipo de accidente -------------------------------------------
+
+#### 2010 - 2019 ####
+
 
 lista_10_19 <- list()
 
@@ -48,9 +54,11 @@ readr::write_rds(lista_acc_1, "data/lista_accidentes_10-19.rds", compress = "gz"
 
 rm(lista_10_19)
 
-# lista_10_19 <- read_rds("data/lista_accidentes_10-19.rds")
+#Recargar lista 
 
-# acc 20 y 21
+lista_acc_1 <- read_rds("data/lista_accidentes_10-19.rds")
+
+#### 2020 - 2021 ####
 
 años_2 <- c(2020, 2021)
 
@@ -75,9 +83,9 @@ rm(lista_20_21)
 
 write_rds(lista_acc_2, "data/lista_accidentes_20-21.rds", compress = "gz")
 
-# lista_acc_2 <- read_rds("data/lista_accidentes_20-21.rds")
+lista_acc_2 <- read_rds("data/lista_accidentes_20-21.rds")
 
-# 2022
+#### 2022 ####
 
 url_22 <- "https://www.carabineros.cl/transparencia/tproactiva/OS2/os2_acc_2022.xlsx"
 tf_22 <- tempfile(fileext = ".xlsx")
@@ -86,8 +94,17 @@ download.file(url_22, tf_22, mode = "wb")
 acc_22 <- read_xlsx(tf_22, sheet = 1)
 
 
-# l_10_21 <- c(lista_10_19, lista_acc_2)
+##### Unión bases #####
 
-# df1 <- do.call(rbind, lista_10_19)
+df10_19 <- do.call(rbind, lista_acc_1) #Con las 2010 -2019 funciona pues son iguales.
+# df20_21 <- do.call(rbind, lista_acc_2) #No funciona con estas porque hay diferencias.
 
-# length(unique(df1$Comuna))
+
+list2env(setNames(lista_acc_2, paste0("df_",años_2)), envir = .GlobalEnv)
+
+df_2020 <- df_2020 %>% 
+  rename("Codcomuna" = Comuna,
+         "Comuna" = Comuna2)
+
+df20_21 <- full_join(df_2020, df_2021) # tira error por las columnas posixct
+
